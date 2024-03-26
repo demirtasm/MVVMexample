1cba6c3113781dedf7b207e7448eec2df459604d
Mukaddes Demirtaş <m.ddemirtas@hotmail.com>
2024-03-26 12:10:06 +0300

    Basic mvvm

---
+++ app/src/main/java/md/learn/counterapp/CounterViewModel.kt
@@ -0,0 +1,23 @@
+package md.learn.counterapp
+
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+
+
+class CounterViewModel : ViewModel() {
+    private val _count = mutableStateOf(0)
+    val count: MutableState<Int> = _count
+
+    fun increment() {
+        _count.value++
+    }
+
+    fun decrement() {
+        _count.value--
+    }
+}
+
